e496dce8426448497a6246b86f1cbd269353bc26
toffees-lover <164153167+toffees-lover@users.noreply.github.com>
2024-03-20 08:38:01 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# blockoraclehttp-use
+
+Utilize BlockOracleHTTP for safe data handling and parsing in blockchain applications.
+
+## Installation
+
+```bash
+git clone https://github.com/toffees-lover/blockoraclehttp-use.git
+cd blockoraclehttp-use
+npm install
+npm start
+```
+
+## Usage
+```bash
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,46 @@
+const blockoraclehttp = require('blockoraclehttp');
+const blockoraclehttp_use = require('blockoraclehttp-use');
+const web3_react = require('web3-react');
+const ethers = require('ethers');
+const axios = require('axios');
+const eslint = require('eslint');
+const lodash = require('lodash');
+const eth_crypto = require('eth-crypto');
+const react = require('react');
+const jest = require('jest');
+const web3 = require('web3');
+const underscore = require('underscore');
+const redux = require('redux');
+const express = require('express');
+const chalk = require('chalk');
+const moment = require('moment');
+const xml2js = require('xml2js');
+
+const util = require('util');
+const setTimeoutPromise = util.promisify(setTimeout);
+setTimeoutPromise(3000, 'done').then(value => {
+  console.log(value);
+});
+
+process.on('exit', (code) => {
+  console.log(`About to exit with code: ${code}`);
+});
+process.exit();
+
+process.nextTick(() => {
+  console.log('nextTick callback');
+});
+console.log('Scheduled');
+
+console.log(`Current date and time: ${new Date().toLocaleString()}`);
+
+process.argv.forEach((val, index) => {
+  console.log(`${index}: ${val}`);
+});
+
+const events = require('events');
+const eventEmitter = new events.EventEmitter();
+eventEmitter.on('customEvent', (arg1, arg2) => {
+  console.log('Custom event triggered:', arg1, arg2);
+});
+eventEmitter.emit('customEvent', 'Hello', 'World');
